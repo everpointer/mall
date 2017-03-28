2852b274ef4a89581d8ff65f7be5826e8edeaf07
qiyueshiyi <qiyueshiyi@outlook.com>
2017-03-28 16:51:02 +0800

    Modify shop home return data handle

--- src/Http/Controllers/HomeController.php
+++ src/Http/Controllers/HomeController.php
@@ -2,16 +2,11 @@
 
 namespace Notadd\Shop\Http\Controllers;
 
-use Notadd\Member\Models\Member;
-use Notadd\Shop\Models\Order;
-use Notadd\Shop\Models\Product;
-use Notadd\Shop\Models\FreeProduct;
-use Illuminate\Support\Facades\Auth;
-use Notadd\Shop\Helpers\ProductsHelper;
+use Notadd\Shop\Http\Handlers\HomeDataHandler;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(HomeDataHandler $handler)
     {
         // $seeder = new \DatabaseSeeder();
         // dd($seeder->run());
@@ -19,55 +14,7 @@ class HomeController extends Controller
         // $members = Member::with('profile')->get()->toArray();
         // dd($members);
 
-        $helperProd = new ProductsHelper();
-
-        $carousel   = $helperProd->suggest('carousel');
-        $viewed     = $helperProd->suggest('viewed', 8);
-        $categories = $helperProd->suggest('categories');
-        $purchased  = $helperProd->suggest('purchased');
-
-        $suggestion = [
-            'carousel'   => $carousel,
-            'viewed'     => $viewed,
-            'categories' => $categories,
-            'purchased'  => $purchased,
-        ];
-
-        $helperProd->resetHaystack(); // reseting session id validator
-
-        $events = [];
-        if (config('shop.offering_free_products')) {
-            $events = FreeProduct::getNextEvents([
-                'id',
-                'description',
-                'min_participants',
-                'max_participants',
-                'participation_cost',
-                'start_date',
-                'end_date',
-            ], 4, date('Y-m-d'));
-        }
-
-        $tagsCloud = ProductsController::getTopRated(0, 20, true);
-
-        $allWishes = '';
-        $user      = Auth::user();
-        if ($user) {
-            $allWishes = Order::ofType('wishlist')->where('user_id', $user->id)->where('description', '<>', '')->get();
-        }
-
-        $banner = [
-            '/img/banner/01.png',
-            '/img/banner/02.png',
-            '/img/banner/03.png',
-            '/img/banner/04.png',
-        ];
-
-        return response()->json([
-            'code'    => 200,
-            'data'    => compact('suggestion', 'allWishes', 'events', 'tagsCloud', 'banner'),
-            'message' => 'success!',
-        ]);
+        return $handler->toResponse()->generateHttpResponse();
     }
 
     private function createTags()
