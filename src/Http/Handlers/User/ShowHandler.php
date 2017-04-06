611352e0ef4904c643f23f027ec6b15368281cb6
qiyueshiyi <qiyueshiyi@outlook.com>
2017-04-06 17:45:49 +0800

    Add data function to User/ShowHandler

--- src/Http/Handlers/User/ShowHandler.php
+++ src/Http/Handlers/User/ShowHandler.php
@@ -9,9 +9,50 @@
 
 namespace Notadd\Shop\Http\Handlers\User;
 
+use Notadd\Member\Models\Member;
+use Illuminate\Support\Facades\Auth;
 use Notadd\Foundation\Passport\Abstracts\DataHandler;
 
 class ShowHandler extends DataHandler
 {
+    public function data()
+    {
+        $user  = Member::findOrFail(Auth::id())->relationsToArray();
 
+        return compact('user');
+    }
+
+    /**
+     * Http code.
+     *
+     * @return int
+     */
+    public function code()
+    {
+        return 200;
+    }
+
+    /**
+     * Errors for handler.
+     *
+     * @return array
+     */
+    public function errors()
+    {
+        return [
+            '查看产品失败！',
+        ];
+    }
+
+    /**
+     * Messages for handler.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            '查看产品成功！',
+        ];
+    }
 }
