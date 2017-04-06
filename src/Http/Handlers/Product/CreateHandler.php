2795fb390bc3ba796951f5eb3357c23f37500d85
qiyueshiyi <qiyueshiyi@outlook.com>
2017-04-06 15:55:52 +0800

    Add data function Product/CreateHandler

--- src/Http/Handlers/Product/CreateHandler.php
+++ src/Http/Handlers/Product/CreateHandler.php
@@ -9,13 +9,48 @@
 
 namespace Notadd\Shop\Http\Handlers\Product;
 
+use Notadd\Shop\Models\Product;
+use Notadd\Shop\Models\Category;
+use Notadd\Shop\Models\ProductDetail;
+use Notadd\Shop\Helpers\FeaturesHelper;
+use Notadd\Shop\Helpers\ProductsHelper;
 use Notadd\Shop\Http\Handlers\ProductHandler;
 
 class CreateHandler extends ProductHandler
 {
     public function data()
     {
+        $product         = Product::find(-50);
+        $features        = ProductDetail::all()->toArray();
+        $arrayCategories = Category::actives()
+            ->lightSelection()
+            ->get()
+            ->toArray();
 
+        $categories = [
+            '' => trans('shop::product.controller.select_category'),
+        ];
+
+        $condition = [
+            'new'         => trans('shop::product.controller.new'),
+            'refurbished' => trans('shop::product.controller.refurbished'),
+            'used'        => trans('shop::product.controller.used'),
+        ];
+
+        $typesProduct = [
+            'item' => trans('shop::product.controller.item'),
+            'key'  => trans('shop::product.globals.digital_item') . ' ' . trans('shop::product.globals.key'),
+        ];
+
+        $typeItem = 'item';
+
+        // categories drop down formatted
+        ProductsHelper::categoriesDropDownFormat($arrayCategories, $categories);
+
+        $oldFeatures     = ProductDetail::oldFeatures([]);
+        $productsDetails = new FeaturesHelper();
+
+        return compact('product', 'features', 'categories', 'condition', 'typeItem', 'typesProduct', 'oldFeatures', 'productsDetails');
     }
 
     /**
