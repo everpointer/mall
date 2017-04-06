739a183d889ee2648e2f95be458fc69544cdbb6e
qiyueshiyi <qiyueshiyi@outlook.com>
2017-04-06 12:27:25 +0800

    Add ShowMyHandler class

---
+++ src/Http/Handlers/Product/ShowMyHandler.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author        Qiyueshiyi <qiyueshiyi@outlook.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime      2017-04-06 12:26
+ */
+
+namespace Notadd\Shop\Http\Handlers\Product;
+
+use Notadd\Shop\Http\Handlers\ProductHandler;
+
+class ShowMyHandler extends ProductHandler
+{
+
+}
