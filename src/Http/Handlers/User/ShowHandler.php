ad2f02f654f00316462f28f0318f638d3e4167e7
qiyueshiyi <qiyueshiyi@outlook.com>
2017-04-06 17:39:33 +0800

    Add ShowHandler class

---
+++ src/Http/Handlers/User/ShowHandler.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author        Qiyueshiyi <qiyueshiyi@outlook.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime      2017-04-06 17:38
+ */
+
+namespace Notadd\Shop\Http\Handlers\User;
+
+use Notadd\Foundation\Passport\Abstracts\DataHandler;
+
+class ShowHandler extends DataHandler
+{
+
+}
