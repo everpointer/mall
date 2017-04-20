c7e8bc83d9752a1ef0c428cd556eb93b86653b69
twilroad <269044570@qq.com>
2017-04-20 11:05:15 +0800

    correct assets path and re-build

--- resources/mixes/administration/config/index.js
+++ resources/mixes/administration/config/index.js
@@ -5,7 +5,7 @@ module.exports = {
     build: {
         env: require('./prod.env'),
         assetsRoot: path.resolve(__dirname, '../dist'),
-        assetsSubDirectory: 'assets/content/administration',
+        assetsSubDirectory: 'assets/shop/administration',
         assetsPublicPath: '/',
         productionSourceMap: true,
         // Gzip off by default as many popular static hosts such as
