5a912b05ab6a19139fc9984503236df27c81f21a
twilroad <269044570@qq.com>
2017-04-20 11:33:10 +0800

    define top sidebar menus

--- resources/mixes/administration/src/mixins/sidebar.js
+++ resources/mixes/administration/src/mixins/sidebar.js
@@ -1,3 +1,39 @@
 export default function (injection) {
-    window.console.log(injection);
+    injection.useSidebar('shop', [
+        {
+            children: [],
+            icon: 'plus',
+            title: '商品',
+        },
+        {
+            children: [],
+            icon: 'plus',
+            title: '店铺',
+        },
+        {
+            children: [],
+            icon: 'plus',
+            title: '订单',
+        },
+        {
+            children: [],
+            icon: 'plus',
+            title: '运营',
+        },
+        {
+            children: [],
+            icon: 'plus',
+            title: '统计',
+        },
+        {
+            children: [],
+            icon: 'plus',
+            title: '促销',
+        },
+        {
+            children: [],
+            icon: 'plus',
+            title: '设置',
+        },
+    ]);
 }
