211f3bbc0cc918343874a098f853aed5e1a356e9
twilroad <269044570@qq.com>
2017-04-20 11:41:46 +0800

    add dashboard page

--- resources/mixes/administration/src/mixins/router.js
+++ resources/mixes/administration/src/mixins/router.js
@@ -1,3 +1,18 @@
+import Dashboard from '../pages/Dashboard';
+import Layout from '../layouts/Layout';
+
 export default function (injection) {
-    window.console.log(injection);
+    injection.useModuleRoute([
+        {
+            children: [
+                {
+                    beforeEnter: injection.middleware.requireAuth,
+                    component: Dashboard,
+                    path: '/',
+                },
+            ],
+            component: Layout,
+            path: '/shop',
+        },
+    ]);
 }
