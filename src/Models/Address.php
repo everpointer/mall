23e59f21fb46919ff5c0b08f7691d5a92758c140
twilroad <269044570@qq.com>
2017-04-24 16:57:18 +0800

    add model for addresses table

---
+++ src/Models/Address.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-04-24 16:54
+ */
+namespace Notadd\Mall\Models;
+
+use Notadd\Foundation\Database\Model;
+use Notadd\Foundation\Member\Member;
+
+/**
+ * Class Address.
+ */
+class Address extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'line1',
+        'line2',
+        'phone',
+        'country',
+        'state',
+        'user_id',
+        'name_contact',
+        'zipcode',
+        'city',
+    ];
+
+    /**
+     * Get the user who owns the address.
+     */
+    public function user() {
+        $this->belongsTo(Member::class);
+    }
+}
