ab029e69c85902b24347d8c89295e5636ca70903
twilroad <269044570@qq.com>
2017-04-24 18:36:44 +0800

    add form data and binding data for configuration page

--- resources/mixes/administration/src/pages/Configuration.vue
+++ resources/mixes/administration/src/pages/Configuration.vue
@@ -7,8 +7,132 @@
                 injection.sidebar.active('mall');
             });
         },
+        data() {
+            return {
+                action: `${window.api}/mall/upload`,
+                form: {
+                    email: '',
+                    logo: '',
+                    phone: '',
+                },
+                loading: false,
+                rules: {
+                    email: [
+                        {
+                            required: true,
+                            type: 'email',
+                            message: '请输入正确的电子邮箱账号',
+                            trigger: 'change',
+                        },
+                    ],
+                },
+            };
+        },
+        methods: {
+            removeLogo() {
+                this.form.logo = '';
+            },
+            uploadBefore() {
+                injection.loading.start();
+            },
+            uploadError(error, data) {
+                const self = this;
+                injection.loading.error();
+                if (typeof data.message === 'object') {
+                    for (const p in data.message) {
+                        self.$notice.error({
+                            title: data.message[p],
+                        });
+                    }
+                } else {
+                    self.$notice.error({
+                        title: data.message,
+                    });
+                }
+            },
+            uploadFormatError(file) {
+                this.$notice.warning({
+                    title: '文件格式不正确',
+                    desc: `文件 ${file.name} 格式不正确，请上传 jpg 或 png 格式的图片。`,
+                });
+            },
+            uploadSuccess(data) {
+                const self = this;
+                injection.loading.finish();
+                self.$notice.open({
+                    title: data.message,
+                });
+                self.form.avatar = data.data.path;
+            },
+            submit() {
+                const self = this;
+                self.loading = true;
+                self.$refs.form.validate(valid => {
+                    if (valid) {
+                    } else {
+                        self.loading = false;
+                        self.$notice.error({
+                            title: '请正确填写设置信息！',
+                        });
+                    }
+                });
+            },
+        },
     };
 </script>
 <template>
-
+    <div class="mall-wrap">
+        <div class="mall-configuration">
+            <card>
+                <p slot="title">商城设置</p>
+                <i-form :label-width="200" :model="form" ref="form" :rules="rules">
+                    <row>
+                        <i-col span="14">
+                            <form-item label="网站logo" prop="logo">
+                                <div class="image-preview" v-if="form.logo">
+                                    <img :src="form.logo">
+                                    <icon type="close" @click.native="removeLogo"></icon>
+                                </div>
+                                <upload :action="action"
+                                        :before-upload="uploadBefore"
+                                        :format="['jpg','jpeg','png']"
+                                        :max-size="2048"
+                                        :on-error="uploadError"
+                                        :on-format-error="uploadFormatError"
+                                        :on-success="uploadSuccess"
+                                        ref="upload"
+                                        :show-upload-list="false"
+                                        v-if="form.logo === ''">
+                                </upload>
+                            </form-item>
+                        </i-col>
+                    </row>
+                    <row>
+                        <i-col span="14">
+                            <form-item label="平台客服联系电话" prop="phone">
+                                <i-input placeholder="请输入平台客服联系电话" v-model="form.phone"></i-input>
+                            </form-item>
+                        </i-col>
+                    </row>
+                    <row>
+                        <i-col span="14">
+                            <form-item label="平台客服电子邮件" prop="email">
+                                <i-input placeholder="请输入平台客服电子邮件" v-model="form.email"></i-input>
+                            </form-item>
+                        </i-col>
+                    </row>
+                    <row>
+                        <i-col span="14">
+                            <form-item>
+                                <i-button :loading="loading" type="primary" @click.native="submit">
+                                    <span v-if="!loading">确认提交</span>
+                                    <span v-else>正在提交…</span>
+                                </i-button>
+                            </form-item>
+                        </i-col>
+                    </row>
+                </i-form>
+            </card>
+        </div>
+    </div>
 </template>
