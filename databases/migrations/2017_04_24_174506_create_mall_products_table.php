0d79b9dfc916ce4003ca8f3638b8eff763331832
twilroad <269044570@qq.com>
2017-04-24 17:46:41 +0800

    add fields of migration file for mall_products table

--- databases/migrations/2017_04_24_174506_create_mall_products_table.php
+++ databases/migrations/2017_04_24_174506_create_mall_products_table.php
@@ -22,6 +22,26 @@ class CreateMallProductsTable extends Migration
     {
         $this->schema->create('mall_products', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('category_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->integer('parent_id')->nullable();    //for combos
+            $table->integer('products_group')->unsigned()->nullable();
+            $table->boolean('status')->default(1);
+            $table->enum('type', array_keys(trans('globals.product_types')));
+            $table->string('name', 100);
+            $table->string('description', 500);
+            $table->double('price', 10, 2);
+            $table->integer('stock');
+            $table->integer('low_stock')->default(0);
+            $table->string('bar_code')->nullable();
+            $table->string('brand', 30)->nullable();
+            $table->enum('condition', array_keys(trans('globals.product_conditions')));
+            $table->json('tags')->nullable();
+            $table->json('features')->nullable();
+            $table->double('rate_val', 10, 2)->nullable();
+            $table->integer('rate_count')->nullable();
+            $table->integer('sale_counts')->unsigned();
+            $table->integer('view_counts')->unsigned();
             $table->timestamps();
         });
     }
