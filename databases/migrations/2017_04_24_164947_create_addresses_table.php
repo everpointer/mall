c5f1b0b44ead9e637f03cae83e7d6b53c9dd55a8
twilroad <269044570@qq.com>
2017-04-24 16:51:35 +0800

    create migration file for addresses table

---
+++ databases/migrations/2017_04_24_164947_create_addresses_table.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @datetime 2017-04-24 16:49:47
+ */
+
+use Notadd\Foundation\Database\Migrations\Migration;
+
+/**
+ * Class CreateAddressesTable.
+ */
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
