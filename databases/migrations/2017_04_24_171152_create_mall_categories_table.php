2dc4518949cc81bd01025c0a9ba32c893e10e19d
twilroad <269044570@qq.com>
2017-04-24 17:20:39 +0800

    add fields to migration file for mall_categories table

--- databases/migrations/2017_04_24_171152_create_mall_categories_table.php
+++ databases/migrations/2017_04_24_171152_create_mall_categories_table.php
@@ -21,6 +21,14 @@ class CreateMallCategoriesTable extends Migration
     {
         $this->schema->create('mall_categories', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('category_id')->unsigned()->nullable();
+            $table->string('name', 60);
+            $table->string('description', 100);
+            $table->string('icon', 60)->nullable();
+            $table->string('image', 100)->nullable();
+            $table->boolean('status')->default(1);
+            $table->enum('type', ['group', 'store'])->default('store');
+            $table->foreign('category_id')->references('id')->on('categories');
             $table->timestamps();
         });
     }
