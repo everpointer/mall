6bea608d5e4c2712f9901290d318bc6b1a33470c
twilroad <269044570@qq.com>
2017-04-24 14:00:22 +0800

    add logic for configuration api

--- src/Handlers/Configuration/SetHandler.php
+++ src/Handlers/Configuration/SetHandler.php
@@ -8,11 +8,44 @@
  */
 namespace Notadd\Mall\Handlers\Configuration;
 
+use Illuminate\Container\Container;
 use Notadd\Foundation\Passport\Abstracts\SetHandler as AbstractSetHandler;
+use Notadd\Foundation\Setting\Contracts\SettingsRepository;
 
 /**
  * Class SetHandler.
  */
 class SetHandler extends AbstractSetHandler
 {
+    /**
+     * @var \Notadd\Foundation\Setting\Contracts\SettingsRepository
+     */
+    protected $settings;
+
+    /**
+     * GetHandler constructor.
+     *
+     * @param \Illuminate\Container\Container                         $container
+     * @param \Notadd\Foundation\Setting\Contracts\SettingsRepository $settings
+     */
+    public function __construct(Container $container, SettingsRepository $settings)
+    {
+        parent::__construct($container);
+        $this->settings = $settings;
+    }
+
+    /**
+     * Execute Handler.
+     *
+     * @return bool
+     * @throws \Exception
+     */
+    public function execute()
+    {
+        $this->settings->set('mall.configuration.logo', $this->request->input('logo'));
+        $this->settings->set('mall.configuration.service.email', $this->request->input('email'));
+        $this->settings->set('mall.configuration.service.phone', $this->request->input('phone'));
+
+        return true;
+    }
 }
