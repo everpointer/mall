48fc2c228ada320445084cee330cfbc71391931b
twilroad <269044570@qq.com>
2017-04-25 16:42:56 +0800

    add some controller classes

---
+++ src/Controllers/Api/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-04-25 16:39
+ */
+namespace Notadd\Mall\Controllers\Api;
+
+use Notadd\Foundation\Routing\Abstracts\Controller;
+
+/**
+ * Class CategoryController.
+ */
+class CategoryController extends Controller
+{
+}
---
+++ src/Controllers/Api/OrderDetailController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-04-25 16:40
+ */
+namespace Notadd\Mall\Controllers\Api;
+
+/**
+ * Class OrderDetailController.
+ */
+class OrderDetailController
+{
+}
