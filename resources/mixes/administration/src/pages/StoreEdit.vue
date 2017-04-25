dc73e5b4d37d7aadf1154fad5e17b5b6ae85f044
TianJin <tfanxi@163.com>
2017-04-25 19:29:39 +0800

    replace store edit content

--- resources/mixes/administration/src/pages/StoreEdit.vue
+++ resources/mixes/administration/src/pages/StoreEdit.vue
@@ -12,77 +12,85 @@
 				<div class="store-information-form">
 					<card>
 						<i-form ref="storeDetail" :model="storeDetail" :rules="ruleValidate" :label-width="204">
-                            <form-item label="店主账号">
-                                {{storeDetail.account}}
-                            </form-item>
-							<form-item label="店铺名称" prop="storeName">
-                                <row>
-                                    <i-col span="9">
+                            <row>
+                                <i-col span="18">
+                                    <form-item label="店主账号">
+                                        {{storeDetail.account}}
+                                    </form-item>
+                                </i-col>
+                            </row>
+                            <row>
+                                <i-col span="18">
+                                    <form-item label="店铺名称" prop="storeName">
                                         <i-input v-model="storeDetail.storeName"></i-input>
-                                    </i-col>
-                                </row>
-							</form-item>
-                            <form-item label="公司名称">
-                                <row>
-                                    <i-col span="9">
+                                    </form-item>
+                                </i-col>
+                            </row>
+                            <row>
+                                <i-col span="18">
+                                    <form-item label="公司名称">
                                         <i-input v-model="storeDetail.companyName"></i-input>
-                                    </i-col>
-                                </row>
-                            </form-item>
-							<form-item label="所在地区">
-                                <row>
-                                    <i-col span="2">
+                                    </form-item>
+                                </i-col>
+                            </row>
+                            <row>
+                                <i-col span="10">
+                                    <form-item label="所在地区">
                                         <i-select v-model="storeDetail.province" placeholder="请选择">
                                             <i-option value="beijing">北京市</i-option>
                                             <i-option value="shanghai">上海市</i-option>
                                             <i-option value="shenzhen">深圳市</i-option>
                                         </i-select>
-                                    </i-col>
-                                </row>
-							</form-item>
-                            <form-item label="店铺地址">
-                                <row>
-                                    <i-col span="9">
+                                    </form-item>
+                                </i-col>
+                            </row>
+                            <row>
+                                <i-col span="18">
+                                    <form-item label="店铺地址">
                                         <i-input v-model="storeDetail.storeAddress"></i-input>
-                                    </i-col>
-                                </row>
-                            </form-item>
-                            <form-item label="开店时间">
-                                {{storeDetail.createTime}}
-                            </form-item>
-                            <form-item label="所属分类">
-                                <row>
-                                    <i-col span="2">
+                                    </form-item>
+                                </i-col>
+                            </row>
+                            <row>
+                                <i-col span="18">
+                                    <form-item label="开店时间">
+                                        {{storeDetail.createTime}}
+                                    </form-item>
+                                </i-col>
+                            </row>
+                            <row>
+                                <i-col span="10">
+                                    <form-item label="所属分类">
                                         <i-select v-model="storeDetail.province" placeholder="请选择">
                                             <i-option value="beijing">北京市</i-option>
                                             <i-option value="shanghai">上海市</i-option>
                                             <i-option value="shenzhen">深圳市</i-option>
                                         </i-select>
-                                    </i-col>
-                                </row>
-                            </form-item>
-                            <form-item label="所属等级">
-                                <row>
-                                    <i-col span="2">
+                                    </form-item>
+                                </i-col>
+                            </row>
+                            <row>
+                                <i-col span="10">
+                                    <form-item label="所属等级">
                                         <i-select v-model="storeDetail.province" placeholder="请选择">
                                             <i-option value="beijing">北京市</i-option>
                                             <i-option value="shanghai">上海市</i-option>
                                             <i-option value="shenzhen">深圳市</i-option>
                                         </i-select>
-                                    </i-col>
-                                </row>
-                            </form-item>
-                            <form-item label="有效期至">
-                                <row>
-                                    <i-col span="2">
+                                    </form-item>
+                                </i-col>
+                            </row>
+                            <row>
+                                <i-col span="10">
+                                    <form-item label="有效期至">
                                         <i-select v-model="storeDetail.province" placeholder="请选择">
                                             <i-option value="beijing">北京市</i-option>
                                             <i-option value="shanghai">上海市</i-option>
                                             <i-option value="shenzhen">深圳市</i-option>
                                         </i-select>
-                                    </i-col>
-                                </row>
-                            </form-item>
+                                    </form-item>
+                                </i-col>
+                            </row>
 						</i-form>
 					</card>
 				</div>
@@ -103,7 +111,7 @@ export default {
                 storeName: '',
                 companyName: '',
                 storeAddress: '',
-                createTime: '2016-12-23',
+                createTime: '2016-18-23',
             },
         };
     },
