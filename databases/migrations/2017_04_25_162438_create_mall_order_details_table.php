ad1e0746126972b20fbe3781411c53c003d5d1bc
twilroad <269044570@qq.com>
2017-04-25 16:25:42 +0800

    add fields of migration file for mall_order_details table

--- databases/migrations/2017_04_25_162438_create_mall_order_details_table.php
+++ databases/migrations/2017_04_25_162438_create_mall_order_details_table.php
@@ -22,6 +22,16 @@ class CreateMallOrderDetailsTable extends Migration
     {
         $this->schema->create('order_details', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('order_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+            $table->double('price', 10, 2);
+            $table->integer('quantity');
+            $table->boolean('status')->default(1);
+            $table->dateTime('delivery_date')->nullable();
+            $table->integer('rate')->nullable();
+            $table->string('rate_comment')->nullable();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('order_id')->references('id')->on('orders');
             $table->timestamps();
         });
     }
