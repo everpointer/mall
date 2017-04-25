44e5a0c1476aa66a53a1bf5f03655d5151ad8de0
twilroad <269044570@qq.com>
2017-04-25 16:34:07 +0800

    create migration file for mall_company_features table

---
+++ databases/migrations/2017_04_25_163330_create_mall_company_features_table.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @datetime 2017-04-25 16:33:30
+ */
+
+use Illuminate\Database\Schema\Blueprint;
+use Notadd\Foundation\Database\Migrations\Migration;
+
+/**
+ * Class CreateMallCompanyFeaturesTable.
+ */
+class CreateMallCompanyFeaturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema->create('mall_company_features', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema->drop('mall_company_features');
+    }
+}
