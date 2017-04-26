fe46c56ab181f144c601a1a7461a8df479b6ad8e
zhanghuan123 <zhhu_123@163.com>
2017-04-26 15:09:21 +0800

    add store_body_header

--- resources/mixes/administration/src/pages/Store.vue
+++ resources/mixes/administration/src/pages/Store.vue
@@ -2,6 +2,26 @@
     import injection from '../helpers/injection';
 
     export default {
+        data() {
+            return {
+                search_category: '',
+                search_word: '',
+                searchList: [
+                    {
+                        value: '店铺名称',
+                        label: '店铺名称',
+                    },
+                    {
+                        value: '商品名称',
+                        label: '商品名称',
+                    },
+                    {
+                        value: '商品分类',
+                        label: '商品分类',
+                    },
+                ],
+            };
+        },
         beforeRouteEnter(to, from, next) {
             next(() => {
                 injection.sidebar.active('mall');
@@ -13,7 +33,25 @@
     <div class="mall-wrap">
         <div class="store">
             <tabs value="name1">
-                <tab-pane label="店铺管理" name="name1">标签一的内容</tab-pane>
+                <tab-pane label="店铺管理" name="name1">
+                    <div class="prompt_box">
+                        <h6>提示</h6>
+                        <p>如果当前时间超过店铺有效期或店铺处于关闭状态，前台将不能继续浏览该店铺，但是店主仍然可以编辑该店铺</p>
+                    </div>
+                    <div class="store_body">
+                        <div class="store_body_header">
+                            <i-button class="export_btn" type="ghost">导出数据</i-button>
+                            <div class="store_body_header_right">
+                                <i-input v-model="search_word">
+                                    <i-select v-model="search_category" slot="prepend">
+                                        <i-option v-for="item in searchList" :value="item.value" :key="item">{{ item.label }}</i-option>
+                                    </i-select>
+                                    <i-button slot="append" type="primary">搜索</i-button>
+                                </i-input>
+                            </div>
+                        </div>
+                    </div>
+                </tab-pane>
                 <tab-pane label="开店申请" name="name2">标签二的内容</tab-pane>
             </tabs>
         </div>
