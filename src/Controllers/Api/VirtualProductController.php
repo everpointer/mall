ebf2aee61da1587e8f7fc3487aee26b2f616c6aa
twilroad <269044570@qq.com>
2017-04-26 14:34:04 +0800

    add router, entry, and handler for virtual-product/list api

---
+++ src/Controllers/Api/VirtualProductController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-04-26 14:32
+ */
+namespace Notadd\Mall\Controllers\Api;
+
+use Notadd\Foundation\Routing\Abstracts\Controller;
+use Notadd\Mall\Handlers\VirtualProduct\ListHandler;
+
+/**
+ * Class VirtualProductController.
+ */
+class VirtualProductController extends Controller
+{
+    /**
+     * @param \Notadd\Mall\Handlers\VirtualProduct\ListHandler $handler
+     *
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse|\Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
+     */
+    public function list(ListHandler $handler)
+    {
+        return $handler->toResponse()->generateHttpResponse();
+    }
+}
--- src/Listeners/RouteRegister.php
+++ src/Listeners/RouteRegister.php
@@ -16,6 +16,7 @@ use Notadd\Mall\Controllers\Api\ConfigurationSearchController;
 use Notadd\Mall\Controllers\Api\OrderController;
 use Notadd\Mall\Controllers\Api\ProductController;
 use Notadd\Mall\Controllers\Api\UploadController;
+use Notadd\Mall\Controllers\Api\VirtualProductController;
 
 /**
  * Class RouteRegister.
@@ -48,6 +49,7 @@ class RouteRegister extends AbstractRouteRegister
             $this->router->post('product/list', ProductController::class . '@list');
             $this->router->post('product/remove', ProductController::class . '@remove');
             $this->router->post('upload', UploadController::class . '@handle');
+            $this->router->post('virtual-product/list', VirtualProductController::class . '@list');
         });
     }
 }
