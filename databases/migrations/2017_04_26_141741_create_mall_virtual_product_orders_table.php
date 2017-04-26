6035b9aa40aba7e936b5527575f5bf9975ae361b
twilroad <269044570@qq.com>
2017-04-26 14:18:36 +0800

    add fields of migration file for mall_virtual_product_orders table

--- databases/migrations/2017_04_26_141741_create_mall_virtual_product_orders_table.php
+++ databases/migrations/2017_04_26_141741_create_mall_virtual_product_orders_table.php
@@ -22,6 +22,12 @@ class CreateMallVirtualProductOrdersTable extends Migration
     {
         $this->schema->create('mall_virtual_product_orders', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('order_id')->unsigned();
+            $table->integer('virtual_product_id')->unsigned();
+            $table->boolean('status')->default(0);
+            $table->string('email')->nullable();
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('virtual_product_id')->references('id')->on('virtual_products');
             $table->timestamps();
         });
     }
