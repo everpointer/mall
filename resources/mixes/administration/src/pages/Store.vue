9c858cce7d490d4700abe5f83798714dbc443202
zhanghuan123 <zhhu_123@163.com>
2017-04-26 15:33:03 +0800

    modify class name

--- resources/mixes/administration/src/pages/Store.vue
+++ resources/mixes/administration/src/pages/Store.vue
@@ -34,16 +34,16 @@
         <div class="store">
             <tabs value="name1">
                 <tab-pane label="店铺管理" name="name1">
-                    <div class="prompt_box">
+                    <div class="prompt-box">
                         <h6>提示</h6>
                         <p>如果当前时间超过店铺有效期或店铺处于关闭状态，前台将不能继续浏览该店铺，但是店主仍然可以编辑该店铺</p>
                     </div>
                     <div class="store_body">
-                        <div class="store_body_header">
-                            <i-button class="export_btn" type="ghost">导出数据</i-button>
-                            <div class="store_body_header_right">
-                                <i-input v-model="search_word">
-                                    <i-select v-model="search_category" slot="prepend">
+                        <div class="store-body-header">
+                            <i-button class="export-btn" type="ghost">导出数据</i-button>
+                            <div class="store-body-header-right">
+                                <i-input v-model="search-word">
+                                    <i-select v-model="search-category" slot="prepend">
                                         <i-option v-for="item in searchList" :value="item.value" :key="item">{{ item.label }}</i-option>
                                     </i-select>
                                     <i-button slot="append" type="primary">搜索</i-button>
