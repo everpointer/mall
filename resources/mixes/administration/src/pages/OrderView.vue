bf11218238b8d0b9db21787551ddbe8fb566c150
zhanghuan123 <zhhu_123@163.com>
2017-04-27 21:31:13 +0800

    add wrap

--- resources/mixes/administration/src/pages/OrderView.vue
+++ resources/mixes/administration/src/pages/OrderView.vue
@@ -10,18 +10,20 @@
     };
 </script>
 <template>
-    <div class="order-view">
-        <div class="view-title">
-            <i-button type="text">
-                <icon type="chevron-left"></icon>
-            </i-button>
-            <span>商品订单—查看</span>
+    <div class="mall-wrap">
+        <div class="order-view">
+            <div class="view-title">
+                <i-button type="text">
+                    <icon type="chevron-left"></icon>
+                </i-button>
+                <span>商品订单—查看</span>
+            </div>
+            <steps :current="1">
+                <step title="已完成" content="这里是该步骤的描述信息"></step>
+                <step title="进行中" content="这里是该步骤的描述信息"></step>
+                <step title="待进行" content="这里是该步骤的描述信息"></step>
+                <step title="待进行" content="这里是该步骤的描述信息"></step>
+            </steps>
         </div>
-        <steps :current="1">
-            <step title="已完成" content="这里是该步骤的描述信息"></step>
-            <step title="进行中" content="这里是该步骤的描述信息"></step>
-            <step title="待进行" content="这里是该步骤的描述信息"></step>
-            <step title="待进行" content="这里是该步骤的描述信息"></step>
-        </steps>
     </div>
 </template>
