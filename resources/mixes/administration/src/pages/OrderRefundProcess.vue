93c27fbee3aba794a18961b1c8f8f28ed39f87a1
TianJin <tfanxi@163.com>
2017-04-27 14:18:19 +0800

    add order refund process content

--- resources/mixes/administration/src/pages/OrderRefundProcess.vue
+++ resources/mixes/administration/src/pages/OrderRefundProcess.vue
@@ -1,7 +1,48 @@
 <template>
     <div class="mall-wrap">
-        <div class="store-refund-process">
+        <div class="order-refund-process">
+            <div class="store-refund-process store-edit">
+                <div class="edit-link-title">
+                    <i-button type="text">
+                        <icon type="chevron-left"></icon>
+                    </i-button>
+                    <span>退款管理—处理</span>
+                </div>
+                <div class="refund-process-content store-information">
+                    <card>
+                        <i-form ref="refundDetail" :model="refundDetail" :rules="ruleValidate" :label-width="197">
+                            <div class="refund-application">
+                                <h5>买家退款申请</h5>
+                                <div class="application-content refund-module">
+                                    <row>
+                                        <i-col span="18">
+                                            <form-item label="店主账号">
+                                                {{refundDetail.applyTime}}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                    <row>
+                                        <i-col span="18">
+                                            <form-item label="商品名称">
+                                                {{refundDetail.goodsname}}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                </div>
+                            </div>
+                            <div class="refund-handel">
 
+                            </div>
+                            <div class="order-information">
+
+                            </div>
+                            <div class="refund-review">
+
+                            </div>
+                        </i-form>
+                    </card>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -10,6 +51,14 @@
     import injection from '../helpers/injection';
 
     export default {
+        data() {
+            return {
+                refundDetail: {
+                    applyTime: '2016-12-21  10:31:59',
+                    goodsname: '****',
+                },
+            };
+        },
         beforeRouteEnter(to, from, next) {
             next(() => {
                 injection.sidebar.active('mall');
