0b4a8c41eb3f5831a4ef3e1f8243634699457992
TianJin <tfanxi@163.com>
2017-04-27 16:07:53 +0800

    add order refund handel content

--- resources/mixes/administration/src/pages/OrderRefundProcess.vue
+++ resources/mixes/administration/src/pages/OrderRefundProcess.vue
@@ -28,10 +28,61 @@
                                             </form-item>
                                         </i-col>
                                     </row>
+                                    <row>
+                                        <i-col span="18">
+                                            <form-item label="退款金额">
+                                                ￥{{refundDetail.refundMoney}}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                    <row>
+                                        <i-col span="18">
+                                            <form-item label="退款原因">
+                                                {{refundDetail.refundReason}}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                    <row>
+                                        <i-col span="18">
+                                            <form-item label="退款说明">
+                                                {{refundDetail.refundDescription}}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                    <row>
+                                        <i-col span="18">
+                                            <form-item label="凭证上传">
+                                                {{refundDetail.refundImg}}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
                                 </div>
                             </div>
                             <div class="refund-handel">
-
+                                <h5>商家退款处理</h5>
+                                <div class="handel-content refund-module">
+                                    <row>
+                                        <i-col span="18">
+                                            <form-item label="审核结果">
+                                                {{refundDetail.handelResult}}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                    <row>
+                                        <i-col span="18">
+                                            <form-item label="处理备注">
+                                                {{refundDetail.handelText}}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                    <row>
+                                        <i-col span="18">
+                                            <form-item label="处理时间">
+                                                {{refundDetail.handelTime}}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                </div>
                             </div>
                             <div class="order-information">
 
@@ -56,6 +107,13 @@
                 refundDetail: {
                     applyTime: '2016-12-21  10:31:59',
                     goodsname: '****',
+                    refundMoney: '99.00',
+                    refundReason: '不要',
+                    refundDescription: 'mm',
+                    refundImg: '',
+                    handelResult: '同意',
+                    handelText: 'jahwuiha',
+                    handelTime: '2016-12-21  10:31:59',
                 },
             };
         },
