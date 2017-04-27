ef001b825b7866fe437ced803a8235e45a05ce53
TianJin <tfanxi@163.com>
2017-04-27 11:10:09 +0800

    add router module

---
+++ resources/mixes/administration/src/pages/OrderRefundProcess.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="mall-wrap">
+        <div class="store-refund-process">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    import injection from '../helpers/injection';
+
+    export default {
+        beforeRouteEnter(to, from, next) {
+            next(() => {
+                injection.sidebar.active('mall');
+            });
+        },
+    };
+</script>
