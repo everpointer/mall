08dfb461f2d1c998cde374facbd9265d4fac0326
zhanghuan123 <zhhu_123@163.com>
2017-04-28 13:22:02 +0800

    add steps

--- resources/mixes/administration/src/pages/OrderView.vue
+++ resources/mixes/administration/src/pages/OrderView.vue
@@ -2,6 +2,32 @@
     import injection from '../helpers/injection';
 
     export default {
+        data() {
+            return {
+                steps: [
+                    {
+                        title: '生成订单',
+                        content: '2016-12-23  13:41:59',
+                    },
+                    {
+                        title: '完成付款',
+                        content: '2016-12-23  13:41:59',
+                    },
+                    {
+                        title: '商家发货',
+                        content: '2016-12-23  13:41:59',
+                    },
+                    {
+                        title: '收货确认',
+                        content: '2016-12-23  13:41:59',
+                    },
+                    {
+                        title: '完成评价',
+                        content: '2016-12-23  13:41:59',
+                    },
+                ],
+            };
+        },
         beforeRouteEnter(to, from, next) {
             next(() => {
                 injection.sidebar.active('mall');
@@ -18,12 +44,24 @@
                 </i-button>
                 <span>商品订单—查看</span>
             </div>
-            <steps :current="1">
-                <step title="已完成" content="这里是该步骤的描述信息"></step>
-                <step title="进行中" content="这里是该步骤的描述信息"></step>
-                <step title="待进行" content="这里是该步骤的描述信息"></step>
-                <step title="待进行" content="这里是该步骤的描述信息"></step>
-            </steps>
+            <div class="view-body">
+                <div class="view-step">
+                    <steps :current="1">
+                        <step v-for="step in steps" :title="step.title" :content="step.content"></step>
+                    </steps>
+                </div>
+                <tabs type="card" :animated="false">
+                    <tab-pane label="订单详情">
+                        <i-form :label-width="80">
+                            <h4>下单/支付</h4>
+                            <form-item label="输入框">
+                                gswgwregwrg
+                            </form-item>
+                        </i-form>
+                    </tab-pane>
+                    <tab-pane label="退货记录">标签二的内容</tab-pane>
+                </tabs>
+            </div>
         </div>
     </div>
 </template>
