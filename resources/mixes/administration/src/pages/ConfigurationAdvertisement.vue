7de7ae2156d1f0fb6b32f1f7379298df7549f43f
TianJin <tfanxi@163.com>
2017-04-28 12:50:50 +0800

    add configuration-advertisement module

--- resources/mixes/administration/src/pages/ConfigurationAdvertisement.vue
+++ resources/mixes/administration/src/pages/ConfigurationAdvertisement.vue
@@ -10,5 +10,23 @@
     };
 </script>
 <template>
-
+    <div class="mall-wrap">
+        <div class="configuration-advertisement">
+            <tabs value="name1">
+                <tab-pane label="广告管理" name="name1">
+                    <card :bordered="false">
+                        <div class="prompt-box">
+                            <p>提示</p>
+                            <p>将广告位调用代码放入前台页面，将显示该广告位的广告</p>
+                        </div>
+                        <div class="advertisement-action">
+                            <i-button class="add-data" type="ghost">+新增数据</i-button>
+                            <i-button class="delete-data" type="ghost">批量删除</i-button>
+                            <i-button type="text" icon="android-sync" class="refresh">刷新</i-button>
+                        </div>
+                    </card>
+                </tab-pane>
+            </tabs>
+        </div>
+    </div>
 </template>
