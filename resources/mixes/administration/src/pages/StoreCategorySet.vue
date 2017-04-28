133895a0199fbf8014a1abf9b0d144e4f1dacec7
eyvv <1009749309@qq.com>
2017-04-28 09:30:38 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	resources/mixes/administration/src/assets/less/main.less

--- resources/mixes/administration/src/pages/StoreCategorySet.vue
+++ resources/mixes/administration/src/pages/StoreCategorySet.vue
@@ -1,51 +1,25 @@
 <template>
     <div class="mall-wrap">
         <div class="store-category-set">
-            <card>
-                <div class="content">
-                    <ul>
-                        <li v-for="set in sets">
-                            <row>
-                                <i-col span="8">
-                                    <row>
-                                        <i-col span="1" class="content-ico">*</i-col>
-                                        <i-col span="4">{{set.name}}</i-col>
-                                        <i-col span="8">
-                                            <input type="text" class="my-input">
-                                        </i-col>
-                                    </row>
-                                </i-col>
-                            </row>
-                        </li>
-                    </ul>
-                    <div class="rank">
-                        <row>
-                            <i-col span="8">
-                                <row>
-                                    <i-col span="5">排序</i-col>
-                                    <i-col span="14">
-                                      <input type="text" class="my-input">
-                                    </i-col>
-                                </row>
-                                <row>
-                                    <i-col span="1"></i-col>
-                                    <i-col span="19">
-                                      <span>数字范围为0~255，数字越小越靠前</span>
-                                    </i-col>
-                                </row>
-                                <row>
-                                    <i-col span="1"></i-col>
-                                    <i-col span="12">
-                                      <button>
-                                        <router-link to="/" class="text-left">确认提交</router-link>
-                                      </button>
-                                    </i-col>
-                                </row>
-                            </i-col>
-                        </row>
-                    </div>
-                </div>
-          </card>
+            <div class="setBox">店铺名称-设置</div>
+                <card>
+                    <i-form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="200">
+                        <form-item label="分类名称" prop="name">
+                            <i-input v-model="formValidate.name" style="width: 268px;height: 36px"></i-input>
+                        </form-item>
+                        <form-item label="保证金额" prop="number">
+                            <i-input v-model="formValidate.number" style="width: 268px;height: 36px"></i-input>
+                        </form-item>
+                        <form-item label="排序" prop="rank">
+                            <i-input v-model="formValidate.rank" style="width: 268px;height: 36px"></i-input><br>
+                            <span class="range">数字范围为0~255，数字越小越靠前</span>
+                        </form-item>
+                        <form-item>
+                            <i-button type="primary" @click="handleSubmit('formValidate')">确认提交</i-button>
+                        </form-item>
+                    </i-form>
+                </card>
+
         </div>
     </div>
 </template>
@@ -53,12 +27,35 @@
     export default {
         data() {
             return {
-                sets: [
-                    { name: '名称分类' },
-                    { name: '保证金额数' },
-                ],
+                formValidate: {
+                    name: '',
+                    number: '',
+                    rank: '',
+                },
+                ruleValidate: {
+                    name: [
+                        { required: true, message: '分类名称不能为空', trigger: 'blur' },
+                    ],
+                    number: [
+                        { required: true, message: '保证金额数不能为空', trigger: 'blur' },
+                    ],
+                },
             };
         },
+        /*methods: {
+            handleSubmit(name) {
+                this.$refs[name].validate((valid) => {
+                    if (valid) {
+                        this.$Message.success('提交成功!');
+                    } else {
+                        this.$Message.error('表单验证失败!');
+                    }
+                });
+            },
+            handleReset(name) {
+                this.$refs[name].resetFields();
+            },
+        },*/
     };
 </script>
 <style>
