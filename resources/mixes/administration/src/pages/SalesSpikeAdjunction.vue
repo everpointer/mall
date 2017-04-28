a60a125ca8276dc3690d32a1ba32071de099a199
zhanghuan123 <zhhu_123@163.com>
2017-04-28 21:40:46 +0800

    modify SalesSpikeAdjunction

--- resources/mixes/administration/src/pages/SalesSpikeAdjunction.vue
+++ resources/mixes/administration/src/pages/SalesSpikeAdjunction.vue
@@ -4,6 +4,7 @@
     export default {
         data() {
             return {
+                loading: false,
                 activityData: {
                     title: '',
                     startTime: '',
@@ -14,12 +15,23 @@
                     title: [
                         {
                             required: true,
-                            message: '姓名不能为空',
+                            message: '标题不能为空',
                             trigger: 'blur',
                         },
                     ],
                 },
             };
+        },
+        methods: {
+            handleSubmit(name) {
+                this.$refs[name].validate(valid => {
+                    if (valid) {
+                        this.$Message.success('提交成功!');
+                    } else {
+                        this.$Message.error('表单验证失败!');
+                    }
+                });
+            },
         },
         beforeRouteEnter(to, from, next) {
             next(() => {
@@ -43,23 +55,42 @@
                     <p>秒杀活动仅设置结束时间，截止时间内多个活动可同时进行，建议标题以分类区分便于管理</p>
                 </div>
                 <i-form :label-width="200" ref="activityValidate" :model="formValidate" :rules="ruleValidate">
-                    <form-item label="活动标题：" prop="title">
-                        <i-input style="width: 268px;" v-model="activityData.title" placeholder=""></i-input>
-                    </form-item>
-                    <form-item label="开始时间：" prop="startTime">
-                        <date-picker type="date" placeholder="选择日期" v-model="activityData.startTime" style="width: 200px"></date-picker>
-                    </form-item>
-                    <form-item label="结束时间：" prop="endTime">
-                        <date-picker type="date" placeholder="选择日期" v-model="activityData.endTime" style="width: 200px"></date-picker>
-                    </form-item>
-                    <form-item label="状态：" prop="endTime">
-                        <i-switch v-model="activityData.status">
-                            <span slot="open">开</span>
-                            <span slot="close">关</span>
-                        </i-switch>
-                    </form-item>
+                    <row>
+                        <i-col span="12">
+                            <form-item label="活动标题：" prop="title">
+                                <i-input v-model="activityData.title" placeholder=""></i-input>
+                            </form-item>
+                        </i-col>
+                    </row>
+                    <row>
+                        <i-col span="12">
+                            <form-item label="开始时间：" prop="startTime">
+                                <date-picker type="date" placeholder="选择日期" v-model="activityData.startTime"></date-picker>
+                            </form-item>
+                        </i-col>
+                    </row>
+                    <row>
+                        <i-col span="12">
+                            <form-item label="结束时间：" prop="endTime">
+                                <date-picker type="date" placeholder="选择日期" v-model="activityData.endTime"></date-picker>
+                            </form-item>
+                        </i-col>
+                    </row>
+                    <row>
+                        <i-col span="12">
+                            <form-item label="状态：" prop="endTime">
+                                <i-switch v-model="activityData.status">
+                                    <span slot="open">开</span>
+                                    <span slot="close">关</span>
+                                </i-switch>
+                            </form-item>
+                        </i-col>
+                    </row>
                     <form-item>
-                        <i-button type="primary">提交</i-button>
+                        <i-button @click.native="handleSubmit('activityValidate')" type="primary">
+                            <span v-if="!loading">确认提交</span>
+                            <span v-else>正在提交…</span>
+                        </i-button>
                     </form-item>
                 </i-form>
             </div>
