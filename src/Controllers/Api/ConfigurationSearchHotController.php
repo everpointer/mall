e9525a7d5ccfc4d7b1d2589cb4c18c85b0063af0
twilroad <269044570@qq.com>
2017-05-03 11:44:22 +0800

    add router, entry, and handler for configuration/search/hot/list api

---
+++ src/Controllers/Api/ConfigurationSearchHotController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-05-03 11:41
+ */
+namespace Notadd\Mall\Controllers\Api;
+
+use Notadd\Foundation\Routing\Abstracts\Controller;
+use Notadd\Mall\Handlers\Configuration\Search\Hot\ListHandler;
+
+/**
+ * Class ConfigurationSearchHotController.
+ */
+class ConfigurationSearchHotController extends Controller
+{
+    /**
+     * @param \Notadd\Mall\Handlers\Configuration\Search\Hot\ListHandler $handler
+     *
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse|\Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
+     */
+    public function list(ListHandler $handler)
+    {
+        return $handler->toResponse()->generateHttpResponse();
+    }
+}
