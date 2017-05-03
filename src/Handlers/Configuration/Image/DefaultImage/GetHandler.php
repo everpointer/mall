978a8a200281d8c09d2f60272a141c8a5ec44349
twilroad <269044570@qq.com>
2017-05-03 14:48:45 +0800

    add router, entry, and handler for configuration/image/default/get api

---
+++ src/Handlers/Configuration/Image/DefaultImage/GetHandler.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-05-03 14:42
+ */
+namespace Notadd\Mall\Handlers\Configuration\Image\DefaultImage;
+
+use Notadd\Foundation\Passport\Abstracts\DataHandler;
+
+/**
+ * Class WatchHandler.
+ */
+class GetHandler extends DataHandler
+{
+}
--- src/Listeners/RouteRegister.php
+++ src/Listeners/RouteRegister.php
@@ -15,6 +15,7 @@ use Notadd\Mall\Controllers\Api\ConfigurationAdvertisementController;
 use Notadd\Mall\Controllers\Api\ConfigurationAdvertisementPositionController;
 use Notadd\Mall\Controllers\Api\ConfigurationController;
 use Notadd\Mall\Controllers\Api\ConfigurationImageController;
+use Notadd\Mall\Controllers\Api\ConfigurationImageDefaultController;
 use Notadd\Mall\Controllers\Api\ConfigurationSearchController;
 use Notadd\Mall\Controllers\Api\ConfigurationSearchHotController;
 use Notadd\Mall\Controllers\Api\OrderController;
@@ -54,6 +55,7 @@ class RouteRegister extends AbstractRouteRegister
             $this->router->post('configuration/advertisement/position/remove', ConfigurationAdvertisementPositionController::class . '@remove');
             $this->router->post('configuration/image/get', ConfigurationImageController::class . '@get');
             $this->router->post('configuration/image/set', ConfigurationImageController::class . '@set');
+            $this->router->post('configuration/image/default/get', ConfigurationImageDefaultController::class . '@get');
             $this->router->post('configuration/search/get', ConfigurationSearchController::class . '@set');
             $this->router->post('configuration/search/set', ConfigurationSearchController::class . '@set');
             $this->router->post('configuration/search/hot/create', ConfigurationSearchHotController::class . '@create');
