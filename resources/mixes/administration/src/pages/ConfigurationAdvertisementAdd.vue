4c4d80258ade4f132d611eefb0ff4ac129de89eb
TianJin <tfanxi@163.com>
2017-05-03 11:43:25 +0800

    adjust page style

--- resources/mixes/administration/src/pages/ConfigurationAdvertisementAdd.vue
+++ resources/mixes/administration/src/pages/ConfigurationAdvertisementAdd.vue
@@ -117,8 +117,8 @@
                         <i-col span="12">
                             <form-item label="选择广告位">
                                 <i-select placeholder="请选择" v-model="addAdPosition.position">
-                                    <i-option v-for="item in position" :value="item.value" :key="item">{{ item.label }}
-                                    </i-option>
+                                    <i-option v-for="item in position" :value="item.value"
+                                              :key="item">{{ item.label }}</i-option>
                                 </i-select>
                             </form-item>
                         </i-col>
@@ -126,16 +126,16 @@
                     <row>
                         <i-col span="12">
                             <form-item label="开始时间" prop="starttime">
-                                <time-picker placeholder="选择时间" type="time" v-model="addAdPosition.starttime" >
-                                </time-picker>
+                                <time-picker placeholder="选择时间" type="time"
+                                             v-model="addAdPosition.starttime"></time-picker>
                             </form-item>
                         </i-col>
                     </row>
                     <row>
                         <i-col span="12">
                             <form-item label="结束时间" prop="endtime">
-                                <time-picker type="time" placeholder="选择时间" v-model="addAdPosition.endtime" >
-                                </time-picker>
+                                <time-picker type="time" placeholder="选择时间"
+                                             v-model="addAdPosition.endtime" ></time-picker>
                             </form-item>
                         </i-col>
                     </row>
