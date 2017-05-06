4a76656f616b5d55d31b0f8f22f756bc3d14ff0c
TianJin <tfanxi@163.com>
2017-05-06 21:42:44 +0800

    add good category edit module content

--- resources/mixes/administration/src/pages/GoodsCategoryEdit.vue
+++ resources/mixes/administration/src/pages/GoodsCategoryEdit.vue
@@ -42,29 +42,34 @@
                 </i-button>
                 <span>店铺管理—店铺编辑</span>
             </div>
+            <card :bordered="false">
+                <i-form ref="editDetail" :model="editDetail" :rules="ruleValidate" :label-width="200">
+                    <div class="basic-information">
+                        <row>
+                            <i-col span="12">
+                                <form-item label="分类名称" prop="typeName">
+                                    <i-input v-model="editDetail.typeName"></i-input>
+                                </form-item>
+                            </i-col>
+                        </row>
+                        <row>
+                            <i-col span="12">
+                                <form-item label="商品展示方式">
+                                    <i-select placeholder="请选择" v-model="editDetail.showStyle">
+                                        <i-option v-for="item in showStyle" :value="item.value"
+                                                  :key="item">{{ item.label }}</i-option>
+                                    </i-select>
+                                    <div class="tip">
+                                        <p>在商品列表页的展示方式</p>
+                                        <p>“颜色”：每个SPU只展示不同个颜色SKU，同一颜色多个SKU只展示一个SKU</p>
+                                        <p>“SPU”：每个SUP只展示一个SKU</p>
+                                    </div>
+                                </form-item>
+                            </i-col>
+                        </row>
+                    </div>
+                </i-form>
+            </card>
         </div>
-        <card :bordered="false">
-            <i-form ref="editDetail" :model="editDetail" :rules="ruleValidate" :label-width="200">
-                <div class="basic-information">
-                    <row>
-                        <i-col span="12">
-                            <form-item label="分类名称" prop="typeName">
-                                <i-input v-model="editDetail.typeName"></i-input>
-                            </form-item>
-                        </i-col>
-                    </row>
-                    <row>
-                        <i-col span="12">
-                            <form-item label="商品展示方式">
-                                <i-select placeholder="请选择" v-model="editDetail.showStyle">
-                                    <i-option v-for="item in showStyle" :value="item.value"
-                                              :key="item">{{ item.label }}</i-option>
-                                </i-select>
-                            </form-item>
-                        </i-col>
-                    </row>
-                </div>
-            </i-form>
-        </card>
     </div>
 </template>
