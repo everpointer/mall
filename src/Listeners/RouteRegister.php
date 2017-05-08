cb456160c580f4931d20aa10babd059454a26b4b
twilroad <269044570@qq.com>
2017-05-08 15:58:06 +0800

    add router, entry, and handler for shop/rate/edit api

--- src/Listeners/RouteRegister.php
+++ src/Listeners/RouteRegister.php
@@ -106,6 +106,7 @@ class RouteRegister extends AbstractRouteRegister
             $this->router->post('shop/remove', ShopController::class . '@remove');
             $this->router->post('shop/restore', ShopController::class . '@restore');
             $this->router->post('shop/rate/create', ShopRateController::class . '@create');
+            $this->router->post('shop/rate/edit', ShopRateController::class . '@edit');
             $this->router->post('shop/rate/list', ShopRateController::class . '@list');
             $this->router->post('specification/create', SpecificationController::class . '@create');
             $this->router->post('specification/edit', SpecificationController::class . '@edit');
