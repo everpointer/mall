f3fa24246b8705676ad8e1acc28250603652d5d6
twilroad <269044570@qq.com>
2017-05-09 14:58:21 +0800

    add model for mall_order_refunds table

---
+++ src/Models/OrderRefund.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-05-09 14:57
+ */
+namespace Notadd\Mall\Models;
+
+use Notadd\Foundation\Database\Model;
+
+/**
+ * Class OrderRefund.
+ */
+class OrderRefund extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [
+    ];
+
+    /**
+     * @var string
+     */
+    protected $table = 'mall_order_refunds';
+}
