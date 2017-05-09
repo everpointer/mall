8ef39640397abbb9e97007631b7a6ba6c964bb42
twilroad <269044570@qq.com>
2017-05-09 13:55:07 +0800

    create migration file for mall_shop_categories table

---
+++ databases/migrations/2017_05_09_135448_create_mall_shop_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @datetime 2017-05-09 13:54:48
+ */
+
+use Illuminate\Database\Schema\Blueprint;
+use Notadd\Foundation\Database\Migrations\Migration;
+
+/**
+ * Class CreateMallShopCategoriesTable.
+ */
+class CreateMallShopCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema->create('mall_shop_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema->drop('mall_shop_categories');
+    }
+}
