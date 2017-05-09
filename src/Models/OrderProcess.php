dc7939651bf69e7728a522b1f8bf9e1ba74dfcea
twilroad <269044570@qq.com>
2017-05-09 15:45:10 +0800

    add model for mall_order_processes table

---
+++ src/Models/OrderProcess.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-05-09 15:44
+ */
+namespace Notadd\Mall\Models;
+
+use Notadd\Foundation\Database\Model;
+
+/**
+ * Class OrderProcess.
+ */
+class OrderProcess extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [
+    ];
+
+    /**
+     * @var string
+     */
+    protected $table = 'mall_order_processes';
+}
