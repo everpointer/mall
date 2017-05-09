7c5f7439900f48f7408ca749e794b6b32841be5b
twilroad <269044570@qq.com>
2017-05-09 12:00:19 +0800

    add router, entry, and handler for shop/category/create api

--- src/Listeners/RouteRegister.php
+++ src/Listeners/RouteRegister.php
@@ -126,6 +126,7 @@ class RouteRegister extends AbstractRouteRegister
             $this->router->post('shop/list', ShopController::class . '@list');
             $this->router->post('shop/remove', ShopController::class . '@remove');
             $this->router->post('shop/restore', ShopController::class . '@restore');
+            $this->router->post('shop/category/create', ShopCategoryController::class . '@create');
             $this->router->post('shop/category/list', ShopCategoryController::class . '@list');
             $this->router->post('shop/rate', ShopRateController::class . '@rate');
             $this->router->post('shop/rate/create', ShopRateController::class . '@create');
