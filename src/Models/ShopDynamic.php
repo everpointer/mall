413e94dfcf1a51d150d4ec164ef4e9e932121231
twilroad <269044570@qq.com>
2017-05-09 15:05:12 +0800

    add model for mall_shop_dynamics table

---
+++ src/Models/ShopDynamic.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-05-09 15:04
+ */
+namespace Notadd\Mall\Models;
+
+use Notadd\Foundation\Database\Model;
+
+/**
+ * Class ShopDynamic.
+ */
+class ShopDynamic extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [
+    ];
+
+    /**
+     * @var string
+     */
+    protected $table = 'mall_shop_dynamics';
+}
