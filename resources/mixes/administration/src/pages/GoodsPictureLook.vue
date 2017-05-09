79b0a495ba0d2e4af205c6d45d8cb364100dc0c4
TianJin <tfanxi@163.com>
2017-05-09 10:57:38 +0800

    add look picture module

---
+++ resources/mixes/administration/src/pages/GoodsPictureLook.vue
@@ -0,0 +1,32 @@
+<script>
+    import injection from '../helpers/injection';
+
+    export default {
+        beforeRouteEnter(to, from, next) {
+            next(() => {
+                injection.sidebar.active('mall');
+            });
+        },
+        methods: {
+            goBack() {
+                const self = this;
+                self.$router.go(-1);
+            },
+        },
+    };
+</script>
+<template>
+    <div class="mall-wrap">
+        <div class="goods-picture-look">
+            <div class="edit-link-title">
+                <i-button type="text" @click.native="goBack">
+                    <icon type="chevron-left"></icon>
+                </i-button>
+                <span>图片管理—全部图片</span>
+            </div>
+            <card :bordered="false">
+
+            </card>
+        </div>
+    </div>
+</template>
